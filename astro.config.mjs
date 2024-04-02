9048f070a470da2547d8f92c9e9abfc248f11060
yu fujioka <fujiokayu@gmail.com>
2024-04-02 21:59:13 +0900

    set up astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
-export default defineConfig({
-  site: "https://lexingtonthemes.com/",
-  integrations: [tailwind(), sitemap()]
-});
+export default defineConfig( {
+  site: 'https://TechRAMENConf.github.io/',
+  outDir: "./docs",
+  base: '/techramenconf.github.io'
+} );
