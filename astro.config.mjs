05747fb23e0f334ce1b17fcec3e81ba1811f75b3
92thunder <r.kunisada661@gmail.com>
2025-03-02 10:26:58 +0900

    2024ビルドが参照できるように変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig( {
   site: 'https://TechRAMENConf.github.io/',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  redirects: {
+    '/2024': '/2024/index.html'
+  }
 } )
