7f6bb95d478a1625876886cce2cc383709212a46
yu fujioka <17287988+fujiokayu@users.noreply.github.com>
2025-05-08 23:47:22 +0900

    remove unnecessary redirect from astro.config.mjs
    またもリダイレクトループの対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig( {
   site: 'https://techramenconf.net/',
-  integrations: [tailwind()],
-  redirects: {
-    '/2024': '/2024/index.html'
-  }
+  integrations: [tailwind()]
 } )
